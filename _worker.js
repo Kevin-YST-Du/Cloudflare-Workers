d2d867815736a4e30c6136b4b782a20e4f508283
Kevin-YST-Du <143725301+Kevin-YST-Du@users.noreply.github.com>
2025-12-20 14:46:25 +0800

    更新

--- _worker.js
+++ _worker.js
@@ -110,7 +110,7 @@ export default {
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <title>WorkerS Pro IDE</title>
+    <title>Workers Pro IDE</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/monaco-editor@0.45.0/min/vs/loader.js"></script>
     <style>
